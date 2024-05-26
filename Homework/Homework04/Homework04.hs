390ba5b7da4d9dd9a2db859f33ea99f5389e9346
johnny5cfw <john@cryptofinancialworld.com>
2024-05-26 19:30:32 +0000

    Still getting my read handed to me. Trying to figure out the proper syntax and how to call the paired lists to check for for in a recursive manner so that it works even if the list were long.

--- Homework/Homework04/Homework04.hs
+++ Homework/Homework04/Homework04.hs
@@ -1,41 +1,18 @@
 -- Question 1
--- Lets say you have the nested values defined bellow. How would you get the value of
+-- Lets say you have the nested values defined below. How would you get the value of
 -- 4 by using only pattern matching in a function?
 
 nested :: [([Int], [Int])]
 nested = [([1,2],[3,4]), ([5,6],[7,8])]
 
--- my answer:
-{-
-findFour :: [([Int], [Int])] -> Int
-findFour (x,_) = If x == 4 Then " There is a 4 in the first position"
-findFour (_,y) = If y == 4 Then " There is a 4 in the second position" 
-findFour [] = error "No 4 found"
+{- 
+Attempt 50, 5-26-2024
+Still getting my but kicked. It seems very innefficient that one would have to repeat the entire structure, especially were it longer, to define each case. There must be a way to call each paired list and then check the next paired list in a recursive fashion. It is simply out of my skillset right now to do so. Perhaps if I study lists in haskell from a few other classes, Ill be able to fill in this educational gap. I feel so stupid. Its probably very simple and Im overthinking it. 
 -}
-
--- Gpt4o answer:
-{-
-findFour :: [([Int], [Int])] -> Int
-findFour [] = error "No 4 found"
-findFour ((xs, ys):rest) =
-    case (xs, ys) of
-        ([], []) -> findFour rest
-        (4:_, _) -> 4
-        (_, 4:_) -> 4
-        (_:xs', _:ys') -> findFour ((xs', ys'):rest)
-        (xs', ys') -> findFour rest
--}
--- Gpt4o answer 2:
-
-findFour :: [([Int], [Int])] -> Int
-findFour [] = error "No 4 found"
-findFour ((xs, ys):rest)
-    | 4 `elem` xs = 4
-    | 4 `elem` ys = 4
-    | otherwise = findFour rest
-
-
-
+findFour :: [([Int], [Int])] -> String
+findFour [] = "No 4 found"
+findFour [x,_] = if x == 4 then "4 in the first positon" 
+findFour [_,y] = if y == 4 then "4 in the second position"
 
 -- Question 2
 -- Write a function that takes a list of elements of any type and, if the list has 3 or more elements, it
